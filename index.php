24edddfb43efc749d9190e3c1ec3734c45b26eeb
renu933333 <64453753+renu933333@users.noreply.github.com>
2021-02-07 16:16:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include("top_nav.php");?>
+	<!-- top navigation bar close  -->
+	<!-- Content start  -->
+	<div class="row">
+	<div class="col-sm-3">
+<marquee direction="up">	<a href="www.kuk.co.in">kuk
+<br>
+The Kurukshetra University was established in 1956 as a unitary residential University and its foundation stone was laid by late Dr. Rajendra Prasad, </a><br>
+	<a href="facebook.com">Facebook is a social networking website where users can post comments, share photographs and post links to news or other interesting content on the web, chat live, and watch short-form video. </a><br>
+	<a href="google.com">Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising ...</a><br>
+	<a href="yahoo.com">Yahoo! is an Internet portal that incorporates a search engine and a directory of World Wide Web sites organized in a hierarchy of topic categories.</a><br>
+	</marquee>
+	</div>
+	      <div class="col-sm-9">
+	<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+    <li data-target="#demo" data-slide-to="4"></li>
+    <li data-target="#demo" data-slide-to="5"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="slider/1.jpg" alt="a">
+    </div>
+    <div class="carousel-item">
+      <img src="slider/2.jpg" alt="b">
+    </div>
+    <div class="carousel-item">
+      <img src="slider/3.jpeg" alt="c">
+    </div>
+	<div class="carousel-item">
+      <img src="slider/4.jpeg" alt="d">
+    </div>
+	<div class="carousel-item">
+      <img src="slider/5.jpeg" alt="e">
+    </div>
+	<div class="carousel-item">
+      <img src="slider/6.jpeg" alt="f">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+	      </div>
+	</div>
+	<!-- content closed --><br>
+	<div class="progress">
+    <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" style="width:40%">Sql 40%</div>
+  </div><br>
+  <div class="progress">
+    <div class="progress-bar bg-info progress-bar-striped progress-bar-animated" style="width:60%"> php 60%</div>
+  </div><br>
+  <div class="progress">
+    <div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" style="width:80%">css 80%</div>
+  </div><br>
+  <div class="progress">
+    <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" style="width:90%">HTML 90%</div>
+  </div><br>
+	<!-- footer start -->
+	
+	<?php include("bottom_footer.php");?>
+	<!-- footer closed -->
+</body>
+</html>
